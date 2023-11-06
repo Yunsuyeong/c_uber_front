698c6f44f1b3e8f513a1f84bfb183f7265be128c
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-11-06 23:03:12 +0900

    2023/11/06(Client Settings)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { gql, useQuery, useReactiveVar } from "@apollo/client";
 import React from "react";
+import { LoggedInRouter } from "./routers/logged-in-router";
 import { LoggedOutRouter } from "./routers/logged-out-router";
+import { isLoggedInVar } from "./apollo";
 
 function App() {
-  return <LoggedOutRouter />;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,30 @@
 import React from "react";
+import { isLoggedInVar } from "../apollo";
+import { useForm } from "react-hook-form";
 
-export const LoggedOutRouter = () => <span>"Logged Out"</span>;
+interface ILoginForm {
+  email: string;
+  password: string;
+}
+
+export const LoggedOutRouter = () => {
+  const { register, handleSubmit } = useForm<ILoginForm>();
+  const onSubmit = () => {
+    isLoggedInVar(true);
+  };
+  return (
+    <div>
+      <h1>Logged Out</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input type="text" {...register("email", { required: true })} />
+          <input
+            type="text"
+            {...register("password", { minLength: 2, required: true })}
+          />
+          <input type="submit" value="Login" />
+        </div>
+      </form>
+    </div>
+  );
+};
